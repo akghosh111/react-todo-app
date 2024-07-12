d8f6cb59c81134703c409a8257861ea13cce2ca6
akghosh111 <ghoshkiran2017@gmail.com>
2024-07-12 19:59:09 +0530

    adding validation to endpoints

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-const express = require('express')
+const express = require('express');
+const { createTodo, updateTodo } = require('./types');
 const app = express()
 const port = 3000
 
 app.use(express.json());
 
 app.post('/todo', (req, res) => {
+    const createPayload = req.body;
+    const parsedPayload = createTodo.safeParse(createPayload);
+    if(!parsedPayload.success){
+        res.status(411).json({
+            msg: "You f*cking send the wrong inputs!"
+        })
+        return;
+    }
   
 })
 
@@ -13,6 +22,14 @@ app.get('/todos', (req, res) => {
 })
 
 app.put('/completed', (req, res) => {
+    const updatePayload = req.body;
+    const parsedPayload = updateTodo.safeParse(updatePayload);
+    if(!parsedPayload.success){
+        res.status(411).json({
+            msg: "You f*cking send the wrong inputs!"
+        })
+        return;
+    }
   
 })
 
